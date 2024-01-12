47cf6ef31926749a1f86405ce2663f4e90198fba
Elna Halilovic <elna.halilovic@diamir.io>
2024-01-12 16:19:32 +0100

    cleanup

--- BookOdyssee/ContentView.swift
+++ BookOdyssee/ContentView.swift
@@ -14,41 +14,8 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     var body: some View {
-        
-//        ReactorView(SearchReactor()) {
-//            SearchView()
-//        }
-        
         HomeView()
-        
-        //        NavigationView {
-        //            List {
-        //                ForEach(items) { item in
-        //                    NavigationLink {
-        //                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-        //                    } label: {
-        //                        Text(item.timestamp!, formatter: itemFormatter)
-        //                    }
-        //                }
-        //                .onDelete(perform: deleteItems)
-        //            }
-        //            .toolbar {
-        //                ToolbarItem(placement: .navigationBarTrailing) {
-        //                    EditButton()
-        //                }
-        //                ToolbarItem {
-        //                    Button(action: addItem) {
-        //                        Label("Add Item", systemImage: "plus")
-        //                    }
-        //                }
-        //            }
-        //            Text("Select an item")
-        //        }
     }
-    
-    
-    
-    
 }
 
 private let itemFormatter: DateFormatter = {
