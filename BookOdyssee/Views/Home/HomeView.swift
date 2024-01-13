33303b717c4a91c5fa942fd2fa70d4897f36478c
Elna Halilovic <elna.halilovic@diamir.io>
2024-01-13 16:55:53 +0100

    add nav to searchview

--- BookOdyssee/Views/Home/HomeView.swift
+++ BookOdyssee/Views/Home/HomeView.swift
@@ -43,15 +43,28 @@ struct HomeView: View {
                             }
                             Spacer()
                         }
-                    }.navigationDestination(for: LocalBook.self) { book in
-                        if let bookId = book.bookId {
-                            ReactorView(BookReactor(dbContext: viewContext)) {
-                                BookView(bookId: bookId)
-                            }
+                    }
+                }
+                .navigationTitle("To Read")
+                .navigationDestination(for: LocalBook.self) { book in
+                    if let bookId = book.bookId {
+                        ReactorView(BookReactor(dbContext: viewContext)) {
+                            BookView(bookId: bookId)
                         }
                     }
                 }
+            }.toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(
+                        destination: ReactorView(SearchReactor()) {
+                            SearchView()
+                        }
+                    ) {
+                        Image(systemName: "plus")
+                    }
+                }
             }
+            
         }
     }
 }
