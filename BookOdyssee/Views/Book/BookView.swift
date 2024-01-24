f260c1d407f1742796ac9d7b1372554c8b1321c4
Elna Halilovic <elna.halilovic@diamir.io>
2024-01-24 20:59:07 +0100

    add description, make view scrollable

--- BookOdyssee/Views/Book/BookView.swift
+++ BookOdyssee/Views/Book/BookView.swift
@@ -18,106 +18,116 @@ struct BookView: View {
     var bookId : String
     
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            if let book = reactor.book {
+        ScrollView {
+            VStack(alignment: .leading) {
                 
-                Group {
-                    Text(book.volumeInfo.title)
-                        .font(.largeTitle)
-                        .frame(alignment: .top)
-                        .padding(.bottom)
+                if let book = reactor.book {
                     
-                    if let image = book.volumeInfo.imageLinks?.thumbnail {
-                        AsyncImage(url: URL(string: image))
-                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 140)
-                            .scaledToFit()
-                    } else {
-                        Image("NoImagePlaceholder")
-                            .resizable()
-                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 140)
-                            .scaledToFit()
-                    }
-                }.frame(
-                    maxWidth: .infinity,
-                    alignment: Alignment.center
-                ).padding(.bottom)
-                
-                
-                Divider()
-                    .padding()
-                
-                if let authors = book.volumeInfo.authors {
-                    DataRow(
-                        label: "Author",
-                        value: getAuthorString(authorList: authors)
-                    )
-                }
-                
-                if let publisher = book.volumeInfo.publisher {
-                    DataRow(label: "Publisher", value: publisher)
-                }
-                
-                DataRow(label: "Published", value: book.volumeInfo.publishedDate)
-                
-                if let pageCount = book.volumeInfo.pageCount {
-                    DataRow(label: "Page count:", value: String(pageCount))
-                }
-                
-                Spacer()
-                
-                VStack() {
-                    if(reactor.readingState == ReadingState.notAdded.description) {
-                        Button(action: {
-                            reactor.send(.addBookToReadingList)
-                        }) {
-                            Text("Add to reading list")
-                                .padding(15)
-                                .frame(maxWidth: .infinity)
-                                .background(Color("Primary"))
-                                .foregroundColor(.white)
+                    Group {
+                        Text(book.volumeInfo.title)
+                            .font(.largeTitle)
+                            .frame(alignment: .top)
+                            .padding(.bottom)
+                        
+                        if let image = book.volumeInfo.imageLinks?.thumbnail {
+                            AsyncImage(url: URL(string: image))
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 140)
+                                .scaledToFit()
+                        } else {
+                            Image("NoImagePlaceholder")
+                                .resizable()
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 140)
+                                .scaledToFit()
                         }
-                    } else {
-                        if(reactor.readingState == ReadingState.toRead.description) {
+                    }.frame(
+                        maxWidth: .infinity,
+                        alignment: Alignment.center
+                    ).padding(.bottom)
+                    
+                    
+                    Divider()
+                        .padding()
+                    
+                    if let authors = book.volumeInfo.authors {
+                        DataRow(
+                            label: "Author",
+                            value: getAuthorString(authorList: authors)
+                        )
+                    }
+                    
+                    if let publisher = book.volumeInfo.publisher {
+                        DataRow(label: "Publisher", value: publisher)
+                    }
+                    
+                    DataRow(label: "Published", value: book.volumeInfo.publishedDate)
+                    
+                    if let pageCount = book.volumeInfo.pageCount {
+                        DataRow(label: "Page count:", value: String(pageCount))
+                    }
+                    
+                    Divider()
+                    
+                    if let description = book.volumeInfo.description {
+                        Text(description)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                    
+                    VStack() {
+                        if(reactor.readingState == ReadingState.notAdded.description) {
                             Button(action: {
-                                reactor.send(.startReadingBook)
+                                reactor.send(.addBookToReadingList)
                             }) {
-                                Text("Start reading")
+                                Text("Add to reading list")
+                                    .padding(15)
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color("Primary"))
+                                    .foregroundColor(.white)
+                            }
+                        } else {
+                            if(reactor.readingState == ReadingState.toRead.description) {
+                                Button(action: {
+                                    reactor.send(.startReadingBook)
+                                }) {
+                                    Text("Start reading")
+                                        .padding(15)
+                                        .frame(maxWidth: .infinity)
+                                        .background(Color("Primary"))
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            
+                            if(reactor.readingState == ReadingState.toRead.description || reactor.readingState == ReadingState.reading.description) {
+                                Button(action: {
+                                    reactor.send(.finishBook)
+                                }) {
+                                    Text("Finish")
+                                        .padding(15)
+                                        .frame(maxWidth: .infinity)
+                                        .background(Color("Primary"))
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            
+                            Button(action: {
+                                reactor.send(.removeBookFromReadingList)
+                            }) {
+                                Text("Remove from reading list")
                                     .padding(15)
                                     .frame(maxWidth: .infinity)
                                     .background(Color("Primary"))
                                     .foregroundColor(.white)
                             }
                         }
-                        
-                        if(reactor.readingState == ReadingState.toRead.description || reactor.readingState == ReadingState.reading.description) {
-                            Button(action: {
-                                reactor.send(.finishBook)
-                            }) {
-                                Text("Finish")
-                                    .padding(15)
-                                    .frame(maxWidth: .infinity)
-                                    .background(Color("Primary"))
-                                    .foregroundColor(.white)
-                            }
-                        }
-                        
-                        Button(action: {
-                            reactor.send(.removeBookFromReadingList)
-                        }) {
-                            Text("Remove from reading list")
-                                .padding(15)
-                                .frame(maxWidth: .infinity)
-                                .background(Color("Primary"))
-                                .foregroundColor(.white)
-                        }
-                    }
-                }.padding(.horizontal)
-                
-                Spacer()
+                    }.padding(.horizontal)
+                    
+                    Spacer()
+                }
+            }.task {
+                reactor.send(.loadBookData(bookId))
             }
-        }.task {
-            reactor.send(.loadBookData(bookId))
         }
     }
 }
